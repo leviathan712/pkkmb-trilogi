3e9f5303e8eec232b3ec3d0781d6cf0314813f5d
Ijat <turamadhanizza@gmail.com>
2024-08-16 19:49:47 +0700

    update punya fajar

--- resources/views/content/logo.blade.php
+++ resources/views/content/logo.blade.php
@@ -1,23 +1,24 @@
-{{-- Fajar --}}
 <section class="relative bg-gradient-to-br from-blue-900 to-indigo-800 text-white overflow-hidden">
     <div class="absolute inset-0 bg-black opacity-50"></div>
-    <div class="absolute inset-0 bg-cover bg-center" style="background-image: url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80');">
+    <div class="absolute inset-0 bg-cover bg-center"
+        style="background-image: url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80');">
     </div>
 
     <div class="container mx-auto px-4 py-14 relative z-10">
 
         <div class="flex flex-col gap-y-10 items-center justify-between">
             <div class="w-full">
-                <div class="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-2xl p-20 shadow-2xl flex flex-col gap-y-10">
+                <div
+                    class="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-2xl p-5 md:p-20 shadow-2xl flex flex-col gap-y-10">
                     <h1 class="text-xl md:text-3xl text-center font-semibold">Makna dan Arti Logo PKKMB Trilogi 2024</h1>
                     <hr>
                     <div class="flex flex-col lg:flex-row gap-x-20 justify-center">
                         <div class="flex items-center justify-center lg:w-[40rem]">
-                            <img src="images/woody.png" class="w-52" alt="">
+                            <img src="images/logo.png" class="w-40 md:w-52" alt="">
                         </div>
                         <div>
                             <h2 class="text-2xl font-semibold mb-6 text-center lg:text-start">MAKNA</h2>
-                            <ul class="space-y-4 list-decimal uppercase text-sm lg:text-base ">
+                            <ul class="space-y-4 list-decimal text-sm lg:text-base text-justify md:text-start">
                                 <li>Rusa adalah hewan yang mampu beradaptasi dengan berbagai lingkungan. Ini dapat
                                     melambangkan kemampuan mahasiswa baru untuk beradaptasi dengan kehidupan kampus yang
                                     baru dan tantangan yang mungkin mereka hadapi.</li>
@@ -35,7 +36,7 @@
                                     Ini menekankan pada pentingnya bekerja sama dan membangun jaringan di antara sesama
                                     mahasiswa.</li>
                                 <li>
-                                    PADA TANDUK RUSA MEMBENTUK ANGKA 24 yang mengartikan bahwa tahun di adakannya pkkmb
+                                    Pada tanduk rusa membentuk angka 24 yang mengartikan bahwa tahun di adakannya pkkmb
                                     pada
                                     tahun 2024
                                 </li>
@@ -45,32 +46,17 @@
                     <hr>
                     <div class="flex flex-col lg:flex-row gap-x-20 justify-center">
                         <div class="flex items-center justify-center lg:w-[40rem] order-2">
-                            <img src="images/woody.png" class="w-52 hidden lg:block" alt="">
+                            <img src="images/logo.png" class="w-52 hidden lg:block" alt="">
                         </div>
                         <div class="order-1">
                             <h2 class="text-2xl font-semibold mb-6 text-center lg:text-start">ARTI</h2>
-                            <ul class="space-y-4 list-decimal uppercase text-sm lg:text-base">
-                                <li>Rusa adalah hewan yang mampu beradaptasi dengan berbagai lingkungan. Ini dapat
-                                    melambangkan kemampuan mahasiswa baru untuk beradaptasi dengan kehidupan kampus yang
-                                    baru dan tantangan yang mungkin mereka hadapi.</li>
-                                <li>Rusa sering dianggap sebagai simbol kelembutan dan kerendahan hati. Ini dapat
-                                    menjadi
-                                    pengingat bagi mahasiswa baru untuk tetap rendah hati dan bersikap baik dalam
-                                    interaksi
-                                    sosial di kampus.</li>
-                                <li>Dalam banyak budaya, rusa juga melambangkan pertumbuhan dan perkembangan. Hal ini
-                                    bisa
-                                    menginspirasi mahasiswa baru untuk terus berkembang, baik secara akademis maupun
-                                    pribadi, selama waktu mereka di universitas.</li>
-                                <li>Rusa biasanya hidup dalam kelompok, mencerminkan pentingnya organisasi dan
-                                    kolaborasi.
-                                    Ini menekankan pada pentingnya bekerja sama dan membangun jaringan di antara sesama
-                                    mahasiswa.</li>
-                                <li>
-                                    PADA TANDUK RUSA MEMBENTUK ANGKA 24 yang mengartikan bahwa tahun di adakannya pkkmb
-                                    pada
-                                    tahun 2024
-                                </li>
+                            <ul class="space-y-4 list-decimal text-sm lg:text-base text-justify md:text-start">
+                                <li>Oranye sering dikaitkan dengan energi, antusiasme, dan kreativitas yang penting
+                                    untuk inovasi.</li>
+                                <li>Hijau selalu melambangkan pertumbuhan, kehidupan, keseimbangan, dan kesehatan.</li>
+                                <li>Biru melambangkan komunikasi dan ketenangan, yang penting dalam kolaborasi.</li>
+                                <li>Merah menjadi simbol yang menginspirasi mahasiswa baru untuk menjalani kehidupan
+                                    akademis mereka dengan penuh semangat dan tekad.</li>
                             </ul>
                         </div>
                     </div>
--- resources/views/content/tujuan.blade.php
+++ resources/views/content/tujuan.blade.php
@@ -1,22 +1,23 @@
-{{-- Fajar --}}
 <section class="relative bg-gradient-to-br from-blue-900 to-indigo-800 text-white overflow-hidden">
     <div class="absolute inset-0 bg-black opacity-50"></div>
-    <div class="absolute inset-0 bg-cover bg-center" style="background-image: url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80');">
+    <div class="absolute inset-0 bg-cover bg-center"
+        style="background-image: url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80');">
     </div>
 
     <div class="container mx-auto px-4 py-14 relative z-10">
 
         <div class="flex flex-col gap-y-10 items-center justify-between">
             <div class="w-full">
-                <div class="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-2xl p-20 shadow-2xl flex flex-col gap-y-10">
+                <div
+                    class="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-2xl p-6 md:p-20 shadow-2xl flex flex-col gap-y-10">
                     <h1 class="text-xl md:text-3xl text-center font-semibold">TUJUAN KHUSUS</h1>
                     <hr>
                     <div class="flex flex-col lg:flex-row gap-x-20 justify-center">
                         <div class="flex items-center justify-center lg:w-[40rem]">
-                            <img src="images/woody.png" class="w-52" alt="">
+                            <img src="images/logo.png" class="w-40 md:w-52" alt="">
                         </div>
                         <div>
-                            <ul class="space-y-4 list-decimal uppercase text-sm lg:text-base ">
+                            <ul class="space-y-4 list-decimal text-sm text-justify md:text-start lg:text-base">
                                 <li>Memperkenalkan kiat sukses belajar dan mengembangkan din di perguruan tinggi melalui
                                     konsepsi dan praktik Merdeka Belajar – Kampus Merdeka;</li>
                                 <li>Menanamkan kesadaran berbangsa, bernegara, bela negara, serta kepedulian terhadap
